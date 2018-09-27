8459bdf7baa456b797d73f3662c72dcd28549d63
Joshua Flores <joshuaaaronflores@gmail.com>
2018-09-26 23:19:53 -0500

    Copy of Assignment1 to continue onto Assignment 2.

---
+++ Assignment2/EpisodeCell.swift
@@ -0,0 +1,27 @@
+//
+//  EpisodeCell.swift
+//  Assignment2
+//
+//  Created by Joshua Aaron Flores Stavedahl on 9/26/18.
+//  Copyright © 2018 Northern Illinois University. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
--- Assignment2/MasterViewController.swift
+++ Assignment2/MasterViewController.swift
@@ -9,5 +9,124 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    
+    var detailViewController: DetailViewController? = nil
+    var episodeObjects = [Episode]()
+    var downloader = Downloader()
+    var image: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        //let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        //navigationItem.rightBarButtonItem = addButton
+        if let split = splitViewController {
+            let controllers = split.viewControllers
+            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        }
+        
+        downloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let object = episodeObjects[indexPath.row]
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.downloader = downloader
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+    
+    // MARK: - Table View
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episodeObjects.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! EpisodeCell
+        
+        /// Create instance of "President" type.
+        let episodeObject : Episode
+        
+        episodeObject = episodeObjects[indexPath.row]
+        
+        
+        downloader.downloadImage(urlString: episodeObject.image.medium) {
+            (image: UIImage?) in
+            cell.logoImageView!.image = image
+            
+        }
+        
+        cell.titleLabel!.text = episodeObject.name
+        cell.subtitleLabel!.text = episodeObject.airdate
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            episodeObjects.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
+    // Download JSON data, decode it, and populate table view
+    func downloadData() {
+        
+        weak var weakSelf = self
+        
+        downloader.downloadData(urlString: "http://api.tvmaze.com/singlesearch/shows?q=black-mirror&embed=episodes") {
+            (data) in
+            
+            guard let jsonData = data else {
+                weakSelf!.presentAlert(title: "Error", message: "Unable to download JSON data")
+                return
+            }
+            
+            do {
+                let showData = try JSONDecoder().decode(ShowData.self, from: jsonData)
+                weakSelf!.episodeObjects = showData.embedded.episodes
+                weakSelf!.tableView.reloadData()
+            } catch {
+                weakSelf!.presentAlert(title: "Error", message: "Invalid JSON downloaded")
+            }
+        }
+    }
+    
+    func presentAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
