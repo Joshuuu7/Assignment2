6c95bf2a9525015c13320df24bf4888168bf3eb1
Joshua Flores <joshuaaaronflores@gmail.com>
2018-09-26 21:19:39 -0500

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Joshua Aaron Flores Stavedahl on 9/26/18.
+//  Copyright © 2018 Northern Illinois University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
